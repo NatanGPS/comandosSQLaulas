29dcc681f7a233799ad194f44895fb5014dadb0d
NatanGPS <natangpss@gmail.com>
2022-05-12 14:03:58 -0300

    Aula 2 do modulo 2

--- banco-inicial.sql
+++ banco-inicial.sql
@@ -1,12 +1,9 @@
-CREATE DATABASE alura;
-
 CREATE TABLE aluno (
     id SERIAL PRIMARY KEY,
 	primeiro_nome VARCHAR(255) NOT NULL,
 	ultimo_nome VARCHAR(255) NOT NULL,
 	data_nascimento DATE NOT NULL
 );
-
 CREATE TABLE categoria (
     id SERIAL PRIMARY KEY,
 	nome VARCHAR(255) NOT NULL UNIQUE
@@ -24,14 +21,78 @@ CREATE TABLE aluno_curso (
 	PRIMARY KEY (aluno_id, curso_id)
 );
 
--- aula 02 treino
-CREATE TABLE categoria (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
+INSERT INTO aluno (primeiro_nome, ultimo_nome, data_nascimento) VALUES (
+    'Vinicius', 'Dias', '1997-10-15'
+), (
+    'Patrícia', 'Freitas', '1996-10-25'
+), (
+    'Diogo', 'Oliveira', '1994-08-27'
+), (
+    'Maria', 'Rosa', '1985-01-01'
 );
 
-CREATE TABLE curso(
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL
-    categoria_id INTEGER NOT NULL REFERENCES categoria(id)
-);
+
+INSERT INTO categoria (nome) VALUES ('Front-End'), ('Programação'), ('Banco de dados'), ('Data Science');
+
+INSERT INTO curso (nome, categoria_id) VALUES
+    ('HTML',1),
+    ('CSS',1),
+    ('JS',1),
+    ('PHP',2),
+    ('Java',2),
+    ('C++',2),
+    ('PostgreSQL',3),
+    ('MySQL',3),
+    ('Oracle',3),
+    ('SQL Server',3),
+    ('SQLite',3),
+    ('Pandas',4),
+    ('Machine Learning',4),
+    ('Power BI',4);
+
+INSERT INTO aluno_curso VALUES (1,4), (1,11), (2,1), (2,2), (3,4), (3,3),(4,4), (4,6),(4,5);
+
+
+
+
+UPDATE categoria SET nome ='Ciencia de dados' WHERE id=4;
+SELECT *
+  FROM curso;
+
+SELECT aluno.primeiro_nome, 
+       aluno.ultimo_nome,
+	   COUNT(aluno_curso.curso_id) numero_cursos
+  FROM aluno
+  JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
+GROUP BY 1,2
+ORDER BY numero_cursos DESC;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
